620bc84b60377ff06fa12eb490155faaf7726811
Marcin <marpk92@gmail.com>
2023-05-17 21:01:26 +0200

    tablica

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ console.log("Hello world")
 let funcA = (a) => {
     console.log(a);
 }
+
+const table = [23, 21, 1, 45, 64, 12]
+
+for(let i = 0; i < table.length; i++) {
+    console.log(table[i]);
+}
