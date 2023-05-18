1718173e885e0278e3a8dcd95492bd7e35f6892d
Marcin <marpk92@gmail.com>
2023-05-18 14:17:05 +0200

    dodanie jquery

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ console.log("Hello world")
 
 let funcA = (a) => {
     console.log(a);
+    $( "#result" ).text(a);
 }
 
 const table = [23, 21, 1, 45, 64, 12]
@@ -9,3 +10,4 @@ const table = [23, 21, 1, 45, 64, 12]
 for(let i = 0; i < table.length; i++) {
     console.log(table[i]);
 }
+
